80252efde390f2e8e30eb9fd795b81cfcf45f051
vyakob <vasya.yakob1996@gmail.com>
2018-11-27 16:49:00 +0200

    Fixed 404

--- src/main/java/asud/controller/MainController.java
+++ src/main/java/asud/controller/MainController.java
@@ -5,6 +5,7 @@ import asud.entity.AutomationSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,14 +19,22 @@ public class MainController {
   private AutomationSystemDAO systemDAO;
 
   @RequestMapping(value = "/" , method = RequestMethod.GET)
-  public String showPage(final Model model) {
+  public String showPageFirst(final Model model) {
+    return show(model,DEFAULT_ID);
+  }
 
+  @RequestMapping(value = "/{id}")
+  public String showPage(final Model model, @PathVariable("id") final long id){
+    return show(model, id);
+  }
+  private String show(final Model model, final long id) {
     final List<AutomationSystem> automationSystems = systemDAO.findAll();
-    final AutomationSystem automationSystem = systemDAO.findOne(DEFAULT_ID);
+    final AutomationSystem automationSystem = systemDAO.findOne(id);
     model.addAttribute("automationSystemsList", automationSystems);
     model.addAttribute("automationSystem", automationSystem);
     return "index";
   }
+
 
   @RequestMapping(value = "/login")
   public String loginPage(){
